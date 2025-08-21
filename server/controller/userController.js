06348f6e976debc06ccdcb3a5e4b2add58356798
Zohaib Karamat <juttzohaib875@gmail.com>
2025-08-21 12:53:18 +0500

    complete post means (create data) api

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -4,7 +4,7 @@ export const create = async(req ,res) =>{
     try {
         const newUser= new userModel(req.body);
         const {email}=newUser;
-        const userExist = await newUser.findOne({email});
+        const userExist = await userModel.findOne({email});
         if (userExist) {
             return res.status(400).json({message: "User already exists"});
         }
@@ -13,4 +13,9 @@ export const create = async(req ,res) =>{
     } catch (error) {
         res.status(500).json({message: error.message})
     }
+}
+
+
+export const getAllUsers = async(req, res) =>{
+    
 }
