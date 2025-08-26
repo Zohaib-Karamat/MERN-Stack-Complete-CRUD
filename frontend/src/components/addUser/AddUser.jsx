9efef599f5437896abaae13f07d2c48f88ffef0b
Zohaib Karamat <juttzohaib875@gmail.com>
2025-08-26 23:33:38 +0500

    user created successfully on frontend

--- frontend/src/components/addUser/AddUser.jsx
+++ frontend/src/components/addUser/AddUser.jsx
@@ -1,22 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AddUser.css";
+import { Link,useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const AddUser = () => {
+  const users={
+    name:"",
+    email:"",
+    address:""
+  };
+  const [user, setUser] = useState(users);
+  const navigate = useNavigate();
+
+  const inputHandler = (e)=>{
+      const { name, value } = e.target;
+      console.log(name,value);
+      setUser({...user,[name]:value});
+  }
+
+  const submitForm = async (e) => {
+      e.preventDefault();
+      await axios.post("http://localhost:8000/api/user", user)
+      .then((response)=>{
+        console.log("User created sucessfully ", response);
+        navigate("/");
+      })
+      .catch((error)=>{
+        console.log(error)
+      })
+    }
+
+  
   return (
     <div className="addUser">
-      <button to="/" type="button" class="btn">
+      <Link to="/" type="button" class="btn">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" color="#090909" fill="none">
     <path d="M3.99982 11.9998L19.9998 11.9998" stroke="#090909" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"></path>
     <path d="M8.99963 17C8.99963 17 3.99968 13.3176 3.99966 12C3.99965 10.6824 8.99966 7 8.99966 7" stroke="#090909" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"></path>
 </svg> Back
-      </button>
+      </Link>
 
       <h3>Add New User</h3>
-      <form className="addUserForm" >
+      <form className="addUserForm" onSubmit={submitForm}>
         <div className="inputGroup">
           <label htmlFor="name">Name:</label>
           <input
             type="text"
+            onChange={inputHandler}
             id="name"
             name="name"
             autoComplete="off"
@@ -27,6 +57,7 @@ const AddUser = () => {
           <label htmlFor="email">E-mail:</label>
           <input
             type="email"
+            onChange={inputHandler}
             id="email"
             name="email"
             autoComplete="off"
@@ -37,6 +68,7 @@ const AddUser = () => {
           <label htmlFor="address">Address:</label>
           <input
             type="text"
+            onChange={inputHandler}
             id="address"
             name="address"
             autoComplete="off"
