dd158df96cf896f71fb9709840bd7365c0bded90
Zohaib Karamat <juttzohaib875@gmail.com>
2025-08-26 12:53:33 +0500

    add and style crud buttons

--- frontend/src/getUsers/User.jsx
+++ frontend/src/getUsers/User.jsx
@@ -1,11 +1,49 @@
 import React from "react";
+import useState from "useState";
 import "./User.css";
 
 const User = () => {
+  const [User, setUser] = useState([]);
+  
   return (
     <div className="userTable mt-3">
       <button type="button" class="btn btn-primary">
-        Add User{" "}
+        Add User
+        <span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+          color="#fbfafa"
+          fill="none"
+        >
+          <path
+            d="M12 7.5C12 9.433 10.433 11 8.5 11C6.567 11 5 9.433 5 7.5C5 5.567 6.567 4 8.5 4C10.433 4 12 5.567 12 7.5Z"
+            stroke="#fbfafa"
+            stroke-width="2"
+          ></path>
+          <path
+            d="M13.5 11C15.433 11 17 9.433 17 7.5C17 5.567 15.433 4 13.5 4"
+            stroke="#fbfafa"
+            stroke-width="2"
+            stroke-linecap="round"
+          ></path>
+          <path
+            d="M13.1429 20H3.85714C2.83147 20 2 19.2325 2 18.2857C2 15.9188 4.07868 14 6.64286 14H10.3571C11.4023 14 12.3669 14.3188 13.1429 14.8568"
+            stroke="#fbfafa"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>
+          <path
+            d="M19 14V20M22 17L16 17"
+            stroke="#fbfafa"
+            stroke-width="2"
+            stroke-linecap="round"
+          ></path>
+        </svg>
+        </span>
       </button>
       <table className="table table-bordered">
         <thead>
@@ -24,48 +62,67 @@ const User = () => {
             <td>Ali</td>
             <td>ali@gmail.com</td>
             <td>Canada</td>
-            <td>
-              
+            <td className="actions">
+              <button type="button" class="btn btn-warning">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                  color="#f6f6f6"
+                  fill="none"
+                >
+                  <path
+                    d="M16.4249 4.60509L17.4149 3.6151C18.2351 2.79497 19.5648 2.79497 20.3849 3.6151C21.205 4.43524 21.205 5.76493 20.3849 6.58507L19.3949 7.57506M16.4249 4.60509L9.76558 11.2644C9.25807 11.772 8.89804 12.4078 8.72397 13.1041L8 16L10.8959 15.276C11.5922 15.102 12.228 14.7419 12.7356 14.2344L19.3949 7.57506M16.4249 4.60509L19.3949 7.57506"
+                    stroke="#f6f6f6"
+                    stroke-width="2"
+                    stroke-linejoin="round"
+                  ></path>
+                  <path
+                    d="M18.9999 13.5C18.9999 16.7875 18.9999 18.4312 18.092 19.5376C17.9258 19.7401 17.7401 19.9258 17.5375 20.092C16.4312 21 14.7874 21 11.4999 21H11C7.22876 21 5.34316 21 4.17159 19.8284C3.00003 18.6569 3 16.7712 3 13V12.5C3 9.21252 3 7.56879 3.90794 6.46244C4.07417 6.2599 4.2599 6.07417 4.46244 5.90794C5.56879 5 7.21252 5 10.5 5"
+                    stroke="#f6f6f6"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                </svg>
+              </button>
 
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="24"
-                height="24"
-                color="#000000"
-                fill="none"
-                
-              >
-                <path
-                  d="M19.5 5.5L18.8803 15.5251C18.7219 18.0864 18.6428 19.3671 18.0008 20.2879C17.6833 20.7431 17.2747 21.1273 16.8007 21.416C15.8421 22 14.559 22 11.9927 22C9.42312 22 8.1383 22 7.17905 21.4149C6.7048 21.1257 6.296 20.7408 5.97868 20.2848C5.33688 19.3626 5.25945 18.0801 5.10461 15.5152L4.5 5.5"
-                  stroke="#141B34"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                />
-                <path
-                  d="M9 11.7349H15"
-                  stroke="#141B34"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                />
-                <path
-                  d="M10.5 15.6543H13.5"
-                  stroke="#141B34"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                />
-                <path
-                  d="M3 5.5H21M16.0555 5.5L15.3729 4.09173C14.9194 3.15626 14.6926 2.68852 14.3015 2.39681C14.2148 2.3321 14.1229 2.27454 14.0268 2.2247C13.5937 2 13.0739 2 12.0343 2C10.9686 2 10.4358 2 9.99549 2.23412C9.89791 2.28601 9.80479 2.3459 9.7171 2.41317C9.32145 2.7167 9.10044 3.20155 8.65842 4.17126L8.05273 5.5"
-                  stroke="#141B34"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                />
-              </svg>
-
-            <button type="button" class="btn btn-warning">
-                
-                </button>
-
+              <button type="button" class="btn btn-danger">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                  color="#f9f8f8"
+                  fill="none"
+                >
+                  <path
+                    d="M19.5 5.5L18.8803 15.5251C18.7219 18.0864 18.6428 19.3671 18.0008 20.2879C17.6833 20.7431 17.2747 21.1273 16.8007 21.416C15.8421 22 14.559 22 11.9927 22C9.42312 22 8.1383 22 7.17905 21.4149C6.7048 21.1257 6.296 20.7408 5.97868 20.2848C5.33688 19.3626 5.25945 18.0801 5.10461 15.5152L4.5 5.5"
+                    stroke="#f9f8f8"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                  ></path>
+                  <path
+                    d="M9 11.7349H15"
+                    stroke="#f9f8f8"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                  ></path>
+                  <path
+                    d="M10.5 15.6543H13.5"
+                    stroke="#f9f8f8"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                  ></path>
+                  <path
+                    d="M3 5.5H21M16.0555 5.5L15.3729 4.09173C14.9194 3.15626 14.6926 2.68852 14.3015 2.39681C14.2148 2.3321 14.1229 2.27454 14.0268 2.2247C13.5937 2 13.0739 2 12.0343 2C10.9686 2 10.4358 2 9.99549 2.23412C9.89791 2.28601 9.80479 2.3459 9.7171 2.41317C9.32145 2.7167 9.10044 3.20155 8.65842 4.17126L8.05273 5.5"
+                    stroke="#f9f8f8"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                  ></path>
+                </svg>
+              </button>
             </td>
           </tr>
         </tbody>
