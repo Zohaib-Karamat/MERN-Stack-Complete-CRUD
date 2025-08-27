8633a7042afb1849bb27d1880f875192a17df1ec
Zohaib Karamat <juttzohaib875@gmail.com>
2025-08-27 13:17:25 +0500

    delete function working successfully on frontend

--- frontend/src/components/getUsers/User.jsx
+++ frontend/src/components/getUsers/User.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./User.css";
 import axios from "axios";
 import { Link } from "react-router-dom";
-
+import toast from "react-hot-toast";
 
 const User = () => {
   const [users, setUsers] = useState([]);
@@ -12,14 +12,24 @@ const User = () => {
       setUsers(response.data);
     };
     fetchData();
-  }, [])
+  }, []);
 
   const deleteUser = async (userId) => {
-    
-  }
+    await axios
+      .delete(`http://localhost:8000/api/delete/user/${userId}`)
+      .then((response) => {
+        console.log(response.data);
+        setUsers((prevUsers) =>
+          prevUsers.filter((user) => user._id !== userId)
+        );
+        toast.success(response.data.message, { position: "top-right" });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div className="userTable mt-3">
-      
       <Link to="/add" type="button" class="btn btn-primary">
         Add User
         <span>
@@ -58,97 +68,111 @@ const User = () => {
           </svg>
         </span>
       </Link>
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th>Serial No.</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Address</th>
-            {/* for crud operations */}
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            users.map((user, index) => {
-              return (
-                <tr>
-            <td>{index+1}</td>
-            <td>{user.name}</td>
-            <td>{user.email}</td>
-            <td>{user.address}</td>
-            <td className="actions">
-              
-              <Link to={`/update/`+user._id} type="button" class="btn btn-warning">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width="24"
-                  height="24"
-                  color="#f6f6f6"
-                  fill="none"
-                >
-                  <path
-                    d="M16.4249 4.60509L17.4149 3.6151C18.2351 2.79497 19.5648 2.79497 20.3849 3.6151C21.205 4.43524 21.205 5.76493 20.3849 6.58507L19.3949 7.57506M16.4249 4.60509L9.76558 11.2644C9.25807 11.772 8.89804 12.4078 8.72397 13.1041L8 16L10.8959 15.276C11.5922 15.102 12.228 14.7419 12.7356 14.2344L19.3949 7.57506M16.4249 4.60509L19.3949 7.57506"
-                    stroke="#f6f6f6"
-                    stroke-width="2"
-                    stroke-linejoin="round"
-                  ></path>
-                  <path
-                    d="M18.9999 13.5C18.9999 16.7875 18.9999 18.4312 18.092 19.5376C17.9258 19.7401 17.7401 19.9258 17.5375 20.092C16.4312 21 14.7874 21 11.4999 21H11C7.22876 21 5.34316 21 4.17159 19.8284C3.00003 18.6569 3 16.7712 3 13V12.5C3 9.21252 3 7.56879 3.90794 6.46244C4.07417 6.2599 4.2599 6.07417 4.46244 5.90794C5.56879 5 7.21252 5 10.5 5"
-                    stroke="#f6f6f6"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  ></path>
-                </svg>
-              </Link>
+      {/* ternary operator */}
+      {users.length === 0 ? (
+        <div className="noData">
+          <h3>No Records To Display</h3>
+          <p>Please Add New User</p>
+        </div>
+      ) : (
+        <div>
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th>Serial No.</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Address</th>
+                {/* for crud operations */}
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user, index) => {
+                return (
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.address}</td>
+                    <td className="actions">
+                      <Link
+                        to={`/update/` + user._id}
+                        type="button"
+                        class="btn btn-warning"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          width="24"
+                          height="24"
+                          color="#f6f6f6"
+                          fill="none"
+                        >
+                          <path
+                            d="M16.4249 4.60509L17.4149 3.6151C18.2351 2.79497 19.5648 2.79497 20.3849 3.6151C21.205 4.43524 21.205 5.76493 20.3849 6.58507L19.3949 7.57506M16.4249 4.60509L9.76558 11.2644C9.25807 11.772 8.89804 12.4078 8.72397 13.1041L8 16L10.8959 15.276C11.5922 15.102 12.228 14.7419 12.7356 14.2344L19.3949 7.57506M16.4249 4.60509L19.3949 7.57506"
+                            stroke="#f6f6f6"
+                            stroke-width="2"
+                            stroke-linejoin="round"
+                          ></path>
+                          <path
+                            d="M18.9999 13.5C18.9999 16.7875 18.9999 18.4312 18.092 19.5376C17.9258 19.7401 17.7401 19.9258 17.5375 20.092C16.4312 21 14.7874 21 11.4999 21H11C7.22876 21 5.34316 21 4.17159 19.8284C3.00003 18.6569 3 16.7712 3 13V12.5C3 9.21252 3 7.56879 3.90794 6.46244C4.07417 6.2599 4.2599 6.07417 4.46244 5.90794C5.56879 5 7.21252 5 10.5 5"
+                            stroke="#f6f6f6"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></path>
+                        </svg>
+                      </Link>
 
-              <button type="button" class="btn btn-danger">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width="24"
-                  height="24"
-                  color="#f9f8f8"
-                  fill="none"
-                >
-                  <path
-                    d="M19.5 5.5L18.8803 15.5251C18.7219 18.0864 18.6428 19.3671 18.0008 20.2879C17.6833 20.7431 17.2747 21.1273 16.8007 21.416C15.8421 22 14.559 22 11.9927 22C9.42312 22 8.1383 22 7.17905 21.4149C6.7048 21.1257 6.296 20.7408 5.97868 20.2848C5.33688 19.3626 5.25945 18.0801 5.10461 15.5152L4.5 5.5"
-                    stroke="#f9f8f8"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                  ></path>
-                  <path
-                    d="M9 11.7349H15"
-                    stroke="#f9f8f8"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                  ></path>
-                  <path
-                    d="M10.5 15.6543H13.5"
-                    stroke="#f9f8f8"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                  ></path>
-                  <path
-                    d="M3 5.5H21M16.0555 5.5L15.3729 4.09173C14.9194 3.15626 14.6926 2.68852 14.3015 2.39681C14.2148 2.3321 14.1229 2.27454 14.0268 2.2247C13.5937 2 13.0739 2 12.0343 2C10.9686 2 10.4358 2 9.99549 2.23412C9.89791 2.28601 9.80479 2.3459 9.7171 2.41317C9.32145 2.7167 9.10044 3.20155 8.65842 4.17126L8.05273 5.5"
-                    stroke="#f9f8f8"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                  ></path>
-                </svg>
-              </button>
-            </td>
-          </tr>
-              )
-          })
-          }
-        
-      </tbody>
-    </table>
-    </div >
+                      <button
+                        onClick={() => deleteUser(user._id)}
+                        type="button"
+                        class="btn btn-danger"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          width="24"
+                          height="24"
+                          color="#f9f8f8"
+                          fill="none"
+                        >
+                          <path
+                            d="M19.5 5.5L18.8803 15.5251C18.7219 18.0864 18.6428 19.3671 18.0008 20.2879C17.6833 20.7431 17.2747 21.1273 16.8007 21.416C15.8421 22 14.559 22 11.9927 22C9.42312 22 8.1383 22 7.17905 21.4149C6.7048 21.1257 6.296 20.7408 5.97868 20.2848C5.33688 19.3626 5.25945 18.0801 5.10461 15.5152L4.5 5.5"
+                            stroke="#f9f8f8"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                          ></path>
+                          <path
+                            d="M9 11.7349H15"
+                            stroke="#f9f8f8"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                          ></path>
+                          <path
+                            d="M10.5 15.6543H13.5"
+                            stroke="#f9f8f8"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                          ></path>
+                          <path
+                            d="M3 5.5H21M16.0555 5.5L15.3729 4.09173C14.9194 3.15626 14.6926 2.68852 14.3015 2.39681C14.2148 2.3321 14.1229 2.27454 14.0268 2.2247C13.5937 2 13.0739 2 12.0343 2C10.9686 2 10.4358 2 9.99549 2.23412C9.89791 2.28601 9.80479 2.3459 9.7171 2.41317C9.32145 2.7167 9.10044 3.20155 8.65842 4.17126L8.05273 5.5"
+                            stroke="#f9f8f8"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                          ></path>
+                        </svg>
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
   );
 };
 
