4ff6aa096e5df2ee1e67c1b379ecefc79941b963
Zohaib Karamat <juttzohaib875@gmail.com>
2025-08-29 22:00:29 +0500

    Fix Vercel deployment issues

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { Toaster } from "react-hot-toast";
 
-// ✅ Import Bootstrap CSS & JS
+// ✅ Import Bootstrap CSS first
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 // Import your custom CSS after Bootstrap to override its styles
 import './App.css';
